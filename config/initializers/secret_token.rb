f22f89afdfa8bbc219affdfdd419a64fe2db8421
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-07-25 12:55:52 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zaphod::Application.config.secret_token = '4c25435cc6445a98faf12dfc2580d16f34a01d87145ce5ef28c4f9f856c5bb9925e71b63e76a612d6ff6b4b19ea281b36608621ec51f6ddedad648c46cd5b1c5'
